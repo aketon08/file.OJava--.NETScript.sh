21fcdee157392bf3ee222b1eeb6359be2d2e23d9
aketon08 <84485391+aketon08@users.noreply.github.com>
2022-06-08 15:06:02 +1200

    Update script.js

--- script.js
+++ script.js
@@ -9,22 +9,22 @@ const ext = [
 function compile(code){
     var stack=[];
     var sus = /(\.\w+)/g
-    var matchnt = /(\w+)\./
-    stack.push(code.split(" "))
+    var matchnt = /[^\.]+/
+    stack.push(code.split(" ").map(token => token.match(matchnt)))
     stack.push(code.match(sus))
+    console.log(stack)
     for(let i = 0; i < stack[0].length; i++){
-        const match=stack[0][i].match(sus)
+        const match=stack[1]
         if(match != null){
             for(let j = 0; j < match.length; j++){
                 if(ext.includes(match[j])){
                     if(match[j] == ".exe"){
-                        console.log(stack[0])    
-                        console.log(stack[0][stack[0].length-1].match(matchnt)[1])
+                            console.log(stack[0][stack[0].length-1][0])
                         stack[0].pop()
                         console.log(stack)    
                     }
                     if(match[j] == ".vbs"){
-                        console.log(parseInt(stack[0][stack[0].length-1].match(matchnt)[1])+parseInt(stack[0][stack[0].length-2]))
+                        console.log(parseInt(stack[0][stack[0].length-1])+parseInt(stack[0][stack[0].length-2]))
                     }
                 }
             }
