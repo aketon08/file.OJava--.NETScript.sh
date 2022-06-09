c8842e60c463c65eae5be86750bf9f6ac15313ce
aketon08 <84485391+aketon08@users.noreply.github.com>
2022-06-09 14:54:10 +1200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const ext = [
     ".exe", ".vbs"
 ]
 
-function compile(code){
+function run(code){
     var stack=[];
     var sus = /(\.\w+)/g
     var matchnt = /[^\.]+/
@@ -19,7 +19,7 @@ function compile(code){
             for(let j = 0; j < match.length; j++){
                 if(ext.includes(match[j])){
                     if(match[j] == ".exe"){
-                            console.log(stack[0][stack[0].length-1][0])
+                        console.log(stack[0][stack[0].length-1][0])
                         stack[0].pop()
                         console.log(stack)    
                     }
@@ -34,6 +34,6 @@ function compile(code){
 
 document.addEventListener("click", evt => {
     if(evt.target.tagName=="BUTTON"){
-        compile(textarea.value)
+        run(textarea.value)
     }
 })
