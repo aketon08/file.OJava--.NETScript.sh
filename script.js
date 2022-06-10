f5a4386c88cdc261f171b9313a7343398a2a015c
aketon08 <84485391+aketon08@users.noreply.github.com>
2022-06-10 14:55:00 +1200

    Tokeniser

--- script.js
+++ script.js
@@ -3,37 +3,48 @@ const title = document.getElementsByTagName("p")[0]
 const inp = document.getElementsByTagName("textarea")[1]
 
 const ext = [
-    ".exe", ".vbs"
+    ".exe", ".vbs", ".rb"
 ]
 
-function run(code){
-    var stack=[];
-    var sus = /(\.\w+)/g
-    var matchnt = /[^\.]+/
-    stack.push(code.split(" ").map(token => token.match(matchnt)))
-    stack.push(code.match(sus))
-    console.log(stack)
-    for(let i = 0; i < stack[0].length; i++){
-        const match=stack[1]
-        if(match != null){
-            for(let j = 0; j < match.length; j++){
-                if(ext.includes(match[j])){
-                    if(match[j] == ".exe"){
-                        console.log(stack[0][stack[0].length-1][0])
-                        stack[0].pop()
-                        console.log(stack)    
-                    }
-                    if(match[j] == ".vbs"){
-                        console.log(parseInt(stack[0][stack[0].length-1])+parseInt(stack[0][stack[0].length-2]))
-                    }
-                }
+function tokenise(code){
+    var token="",
+        tokens=[],
+        index=0;
+    for(var char of code.split("")){
+        // Checking for spaces and escaping them
+        if(char == " "){
+            if(token[token.length-1] == "\\"){
+                console.log(token)
+                token+=char;
+            } else {
+                tokens.push(token.replace(/\\/g, ''))
+                token="";
             }
         } 
+        else {
+            token+=char;
+        }
+
+        if(index == code.length-1){
+            tokens.push(token.replace(/\\/g, ''))
+        }
+
+        //Checking for file extension
+        if(char == "."){
+            if(token[token.length-1]!="\\"){
+                tokens.push(token)
+                token=""
+            }
+        }
+
+        index++;
     }
+    console.log(tokens)
 }
 
 document.addEventListener("click", evt => {
     if(evt.target.tagName=="BUTTON"){
-        run(textarea.value)
+        tokenise(textarea.value)
+        //console.log(textarea.value)
     }
 })
