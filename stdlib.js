7bd5d8489f0c0c724b2a355fbe047caf30f689d8
aketon08 <84485391+aketon08@users.noreply.github.com>
2022-06-29 13:56:40 +1200

    Update stdlib.js

--- stdlib.js
+++ stdlib.js
@@ -1,8 +1,11 @@
 let output = document.getElementById("output")
 var outp = [];
-var stdlib = {
-    ".exe": (stack, code) => { if(outp.length>0) {output.value+=outp+" "; if(stack.length){stack.pop()}} if (stack.length) { {output.value+=stack[stack.length-1]+" "; stack.pop()} } },
-    ".vbs": (stack, code) => { if (code.includes(".exe")) { outp=[];outp.push(stack[stack.length-2]+stack[stack.length-1] );stack.pop() } else { output.value+=stack[stack.length-2]+stack[stack.length-1] } stack.pop() }, 
-    ".rb":  (stack, code) => { if (code.includes(".exe")) { outp=[];outp.push(stack[stack.length-2]-stack[stack.length-1] );stack.pop() } else { output.value+=stack[stack.length-2]-stack[stack.length-1] } stack.pop() },
-    ".vvvvvv": (stack) => { outp=stack[stack.length-1].toLowerCase() }
+const stdlib = {
+    ".exe"   : (stack, code) => { if(outp.length>0) {output.value+=outp+" "; if(stack.length){stack.pop()}} if (stack.length) { {output.value+=stack[stack.length-1]+" "; stack.pop()} } },
+    ".vbs"   : (stack, code) => { if(code.includes(".exe")) { outp=[];outp.push(stack[stack.length-2]+stack[stack.length-1] );stack.pop() } else { output.value+=stack[stack.length-2]+stack[stack.length-1] } stack.pop() }, 
+    ".rb"    : (stack, code) => { if(code.includes(".exe")) { outp=[];outp.push(stack[stack.length-2]-stack[stack.length-1] );stack.pop() } else { output.value+=stack[stack.length-2]-stack[stack.length-1] } stack.pop() },
+    ".ts"    : (stack, code) => { if(code.includes(".exe")) { outp=[];outp.push(stack[stack.length-2]*stack[stack.length-1] );stack.pop() } else { output.value+=stack[stack.length-2]*stack[stack.length-1] } stack.pop() },
+    ".tar"   : (stack, code) => { if(code.includes(".exe")) { outp=[];outp.push(stack[stack.length-2]/stack[stack.length-1] );stack.pop() } else { output.value+=stack[stack.length-2]/stack[stack.length-1] } stack.pop() },
+    ".vvvvvv": (stack)       => { outp=stack[stack.length-1].toLowerCase() },
+    ".r"     : (stack)       => { var compiled=[]; compiled+=(`for(var ${stack[stack.length-2]} = 0; ${stack[stack.length-2]} < ${stack[stack.length-1]}; ${stack[stack.length-2]}++) {console.log("test", ${stack[stack.length-2]})}`); eval(compiled) }
 }
