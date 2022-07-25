1a54103923d1ec4aac30adb4a09a2fd327d0e00d
aketon08 <84485391+aketon08@users.noreply.github.com>
2022-07-25 18:19:05 +1200

    Changy wangys
    I only had the brain capacity to change one of them don't ask why

--- stdlib.js
+++ stdlib.js
@@ -1,9 +1,29 @@
 let output = document.getElementById("output")
-var outp = [];
 var iterateVar;
+outp=[];
+function math(operand1, operand2, operator, stack=[]){
+    switch (operator) {
+        case "+":
+            outp.push( operand1 + operand2 );
+        case "-":
+            outp.push( operand1 - operand2 );
+        case "*":
+            outp.push( operand1 * operand2 );
+        case "/":
+            outp.push( operand1 / operand2 );
+        case "%":
+            outp.push( operand1 % operand2 );
+        case "^":
+            outp.push( Math.pow(operand1, operand2) );
+        default:
+            outp.push( "Invalid operator" );
+    }
+    stack.pop()+stack.pop();
+    return outp[0];
+}
 const stdlib = {
     ".txt"   : (stack)       => { compiled+=`output.value+=(${stack[stack.length-1]});` },
-    ".vbs"   : (stack)       => { outp=[]; outp.push(stack[stack.length-2]+stack[stack.length-1]); stack.pop()+stack.pop();stack.push(outp[0]) }, 
+    ".vbs"   : (stack)       => { stack.push(math(stack[stack.length-2], stack[stack.length-1], "+", stack)); }, 
     ".rb"    : (stack, code) => { outp=[]; outp.push(stack[stack.length-2]-stack[stack.length-1]); stack.pop()+stack.pop();stack.push(outp[0]) },
     ".ts"    : (stack, code) => { outp=[]; outp.push(stack[stack.length-2]*stack[stack.length-1]); stack.pop()+stack.pop();stack.push(outp[0]) },
     ".tar"   : (stack, code) => { outp=[]; outp.push(stack[stack.length-2]/stack[stack.length-1]); stack.pop()+stack.pop();stack.push(outp[0]) },
